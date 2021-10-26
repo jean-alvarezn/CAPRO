7278aee285486c64161c41aa04e177b111abd43d
jalvarez <wolowizard720@gmail.com>
2021-10-26 18:34:36 -0300

    Se volvio a estructurar el sitio que se estaba haciendo y se comenzo de nuevo

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+let navEnlaces = document.getElementById("navEnlaces");
+
+function mostrarMenu()
+{
+    navEnlaces.style.right = "0";
+}
+
+function esconderMenu()
+{
+    navEnlaces.style.right = "-200px";
+}
