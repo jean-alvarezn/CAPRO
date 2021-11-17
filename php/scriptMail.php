f8c6e09bd05e4dd821a7785c44dd981ed3b1ce9e
jalvarez <wolowizard720@gmail.com>
2021-11-17 12:47:40 -0300

    Cambios al php

--- php/scriptMail.php
+++ php/scriptMail.php
@@ -1,10 +1,24 @@
 <?php
 
 $nombre = $_POST['nombre'];
+$empresa = $_POST['empresa'];
 $correo_remitente = $_POST['correo_remitente'];
 $telefono = $_POST['telefono'];
 $asunto = $_POST['asunto'];
 $mensaje = $_POST['mensaje'];
+
+if(isset($_POST['submit']))
+{
+    if(!empty($_POST['eleccion']))
+    {
+        foreach($_POST['eleccion'] as $valor)
+        {
+         $seleccion = $_POST['valor'];   
+        }
+    }
+}
+
+
 
 
 $correo_de = 'jalvarez@dieselomt.cl';
@@ -13,6 +27,7 @@ $correo_cuerpo = "Nombre: $nombre.\n".
                  "Correo: $correo_remitente.\n".
                  "Asunto: $asunto.\n".
                  "Telefono: $telefono.\n".
+                 "Tipo de Contacto: $seleccion .\n".
                  "Mensaje: $mensaje .\n".;
 $para = 'wolowizard720@gmail.com';
 $cabeceras = "De: $correo_de \r\n";
